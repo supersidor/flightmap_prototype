3c869797abc3cb3218efe40451810f9ed38f5337
Andriy Sydorka <sidorka@gmail.com>
2021-01-07 22:04:22 +0200

    websocket

---
+++ server/src/main/java/com/supersidor/flightmap/websocket/Event.java
@@ -0,0 +1,11 @@
+package com.supersidor.flightmap.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Event {
+    private String eventId;
+    private String eventDt;
+}
--- web/src/main/ng/src/app/home-page/home-page.component.ts
+++ web/src/main/ng/src/app/home-page/home-page.component.ts
@@ -18,6 +18,18 @@ export class HomePageComponent implements OnInit {
       console.log(user)
       this.user = user;
     });
+    let socket = new WebSocket('ws://localhost:8080/ws');
+// Connection opened
+    socket.addEventListener('open', function (event) {
+       console.log("send ")
+       socket.send('Hello Server!');
+    });
+
+// Listen for messages
+    socket.addEventListener('message', function (event) {
+         console.log('Message from server ', event);
+    });
+
   }
 
 }
