2a547fc60e6730e4fdfde58c0e12d69182e4728f
Andriy Sydorka <you@example.com>
2021-01-23 12:54:19 +0200

    rewritten to rxjs

--- stresstest/src/app.ts
+++ stresstest/src/app.ts
@@ -1,91 +1,126 @@
 import {TestUser} from "./test-user";
 import {Position} from "./position";
 import {computeDestinationPoint} from "geolib";
+import {combineLatest, from, Observable, of, range} from "rxjs";
+import {concatMap, delay, finalize, flatMap, map, mergeMap, tap, withLatestFrom} from "rxjs/operators";
+import {AxiosResponse} from "axios";
 
 const serverUrl = 'http://localhost:8080/'
 const axios = require('axios').default
 //let TestUser = require("./test-user");
 var W3CWebSocket = require('websocket').w3cwebsocket;
 
-async function getUser(email:string) : Promise<TestUser>{
-    return axios.get(serverUrl+'api/test/user', {
+function getUser(email: string): Observable<TestUser> {
+    let promise: Promise<AxiosResponse<TestUser>> = axios.get(serverUrl + 'api/test/user', {
         params: {
             userName: email
         }
-    }).then ( (resp) => Promise.resolve(resp.data))
+    });
+    return from(promise).pipe(
+        map(resp => resp.data)
+    )
 }
-function getAircraft(testData: TestUser, airCraftName: string): Promise<number> {
-    return axios.get(serverUrl+'api/aircraft/register', {
+
+function getAircraft(testData: TestUser, airCraftName: string): Observable<number> {
+    let promise: Promise<AxiosResponse<number>> = axios.get(serverUrl + 'api/aircraft/register', {
         params: {
             name: airCraftName
         },
         headers: {
             Authorization: 'Bearer '.concat(testData.token)
         }
-    }).then ( (resp) => Promise.resolve(resp.data))
+    })
+    return from(promise).pipe(
+        map(resp => resp.data)
+    )
 }
 
-// axios.get('http://localhost:8080/api/test/user', {
-//     params: {
-//         userName: 'test1@flight.test'
-//     }
-// }).then(function (response) {
-//     console.log("userresponse.data);
-// }).catch(function (error) {
-//     console.log(error);
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-async function produceData(testData: TestUser, airCraftId: number): Promise<String>{
-    var client = new W3CWebSocket('ws://localhost:8080/console?token='+testData.token);
-    client.onmessage = function(e) {
-        console.log("Received: '",e.data);
+
+function produceDataToSocket(socket, testData: TestUser, airCraftId: number): Observable<String> {
+    let pos: Position = {
+        aircraftId: airCraftId,
+        longitude: getRandomInt(-180, 180),
+        latitude: 0,
+        altitude: 1000,
+        heading: getRandomInt(0, 360),
+        timestamp: 0
+    }
+    const speedMetersPerSec = 300 * 1000 / (60 * 60);
+
+    return range(1, 1000).pipe(
+        concatMap(idx => of(idx).pipe(delay(1000))),
+        mergeMap((idx) => {
+                    pos.timestamp = new Date().getTime();
+                    let nextPos = computeDestinationPoint(pos, speedMetersPerSec, pos.heading);
+                    pos.latitude = nextPos.latitude;
+                    pos.longitude = nextPos.longitude;
+
+                    let posJson = JSON.stringify(pos);
+                    //console.log("Sending", testData.user.email, idx)
+                    socket.send(posJson);
+                    return of("send "+idx +" to "+testData.user.email);
+        })
+    )
+}
+
+function produceData(testData: TestUser, airCraftId: number): Observable<String> {
+
+    var client = new W3CWebSocket('ws://localhost:8080/console?token=' + testData.token);
+    client.onmessage = function (e) {
+        console.log("Received: '", e.data);
     };
-    client.onopen = function() {
+    client.onopen = function () {
         console.log('WebSocket Client Connected');
-
-        // function sendNumber() {
-        //     if (client.readyState === client.OPEN) {
-        //         var number = Math.round(Math.random() * 0xFFFFFF);
-        //         client.send(number.toString());
-        //         setTimeout(sendNumber, 1000);
-        //     }
-        // }
-        // sendNumber();
     };
-    const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-    while (client.readyState==0){
-        await snooze(500);
+    function waitForSocketToOpen(socket) {
+        return new Promise(function (resolve, reject) {
+            (function waitForSocket() {
+                if (client.readyState != 0) {
+                    return resolve();
+                }
+                setTimeout(waitForSocket, 500);
+            })();
+        });
     }
-    console.log()
-    const speedMetersPerSec =  300*1000/ (60*60);
-    if (client.readyState==1){
-        let pos:Position = {
-            aircraftId:airCraftId,
-            longitude: getRandomInt(-180,180),
-            latitude: 0,
-            altitude: 1000,
-            heading:getRandomInt(0,360),
-            timestamp: 0
-        }
-        for (var i=0;i<1000;i++){
-            pos.timestamp = new Date().getTime();
-            let nextPos = computeDestinationPoint(pos,speedMetersPerSec,pos.heading);
-            pos.latitude = nextPos.latitude;
-            pos.longitude = nextPos.longitude;
 
-            let posJson = JSON.stringify(pos);
-            console.log("Sending",testData.user.email,i)
-            client.send(posJson);
-            await snooze(1000)
-        }
-        client.close()
-    }else{
-        console.log("Failed to open socket");
-    }
+    return from(waitForSocketToOpen(client)).pipe(
+        mergeMap(
+            () => produceDataToSocket(client, testData, airCraftId)
+        ),
+        finalize(() => client.close())
+    )
+
+
+    // const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+    //
+    // while (client.readyState == 0) {
+    //     await snooze(500);
+    // }
+    // console.log()
+    // const speedMetersPerSec = 300 * 1000 / (60 * 60);
+    // if (client.readyState == 1) {
+
+    //     for (var i = 0; i < 1000; i++) {
+    //         pos.timestamp = new Date().getTime();
+    //         let nextPos = computeDestinationPoint(pos, speedMetersPerSec, pos.heading);
+    //         pos.latitude = nextPos.latitude;
+    //         pos.longitude = nextPos.longitude;
+    //
+    //         let posJson = JSON.stringify(pos);
+    //         console.log("Sending", testData.user.email, i)
+    //         client.send(posJson);
+    //         await snooze(1000)
+    //     }
+    //     client.close()
+    // } else {
+    //     console.log("Failed to open socket");
+    // }
 
 
     //
@@ -94,30 +129,48 @@ async function produceData(testData: TestUser, airCraftId: number): Promise<Stri
     //     err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
     //     () => console.log('complete') // Called when connection is closed (for whatever reason).
     // );
-    return Promise.resolve("ok");
+    //return Promise.resolve("ok");
 }
 
 // })
-async function  main(){
-    //let users = new Array<User>();
-    for (var i=0;i<1;i++){
-        const email = `test${i}@flight.test1`;
-        getUser(email).then( testData => {
-            console.log(`email:${testData.user.email} id:${testData.user.id}`);
-            return getAircraft(testData,"test aircraft "+testData.user.email).then(
-                (airCraftId) => {
-                    console.log(`email:${testData.user.email} airCraftId:${testData.user.id}`);
-                    return produceData(testData,airCraftId)
-                }
-            )
-        }).then( (status) => {
-            console.log("status:",status);
-        })
-    }
+function main() {
+    // //let users = new Array<User>();
+    // for (var i=0;i<1;i++){
+    //     const email = `test${i}@flight.test1`;
+    //     getUser(email).then( testData => {
+    //         console.log(`email:${testData.user.email} id:${testData.user.id}`);
+    //         return getAircraft(testData,"test aircraft "+testData.user.email).then(
+    //             (airCraftId) => {
+    //                 console.log(`email:${testData.user.email} airCraftId:${testData.user.id}`);
+    //                 return produceData(testData,airCraftId)
+    //             }
+    //         )
+    //     }).then( (status) => {
+    //         console.log("status:",status);
+    //     })
+    // }
+    // range(1, 10).pipe(
+    //     delay(10000)
+    // ).subscribe(
+    //     x => console.log(x)
+    // )
+    // return;
+    range(1, 1000).pipe(
+        mergeMap(idx => getUser(`test${idx}@flight.test2`)),
+        mergeMap(testData => {
+            return combineLatest([
+                of(testData), getAircraft(testData, "test aircraft " + testData.user.email)
+            ])
+        }),
+        mergeMap(tuple => produceData(tuple[0], tuple[1]))
+    ).subscribe(
+        x => console.log(x)
+    );
 }
 
+main()
 
-const callMain = async () => {
-    await main()
-};
-callMain()
+// const callMain = async () => {
+//     await main()
+// };
+// callMain()
