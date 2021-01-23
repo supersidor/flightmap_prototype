2d2f75352a1425fb2a0479be2e836c3fb9d9e89e
Andriy Sydorka <you@example.com>
2021-01-23 19:24:56 +0200

    progress kotlin

--- server/src/main/java/com/supersidor/flightmap/util/CookieUtility.kt
+++ server/src/main/java/com/supersidor/flightmap/util/CookieUtility.kt
@@ -1,42 +1,36 @@
-package com.supersidor.flightmap.util;
+package com.supersidor.flightmap.util
 
-import org.springframework.http.HttpCookie;
-import org.springframework.http.ResponseCookie;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
-
-import java.util.List;
+import org.springframework.http.HttpCookie
+import org.springframework.util.MultiValueMap
+import org.springframework.http.ResponseCookie
+import org.springframework.http.server.reactive.ServerHttpResponse
+import org.springframework.stereotype.Component
 
 @Component
-public class CookieUtility {
-    public String getCookieValue(MultiValueMap<String, HttpCookie> cookieMap, String cookieName) {
-        List<HttpCookie> cookies = cookieMap.get(cookieName);
-        if (cookies == null) {
-            return null;
-        }
-
-        return cookies.stream().findFirst().map(HttpCookie::getValue).orElse(null);
+class CookieUtility {
+    fun getCookieValue(cookieMap: MultiValueMap<String?, HttpCookie>, cookieName: String?): String? {
+        val cookies = cookieMap[cookieName] ?: return null
+        return cookies.stream().findFirst().map { obj: HttpCookie -> obj.value }.orElse(null)
     }
 
-    public void deleteCookie(ServerHttpResponse response, MultiValueMap<String, HttpCookie> cookieMap, String cookieName) {
-        List<HttpCookie> cookies = cookieMap.get(cookieName);
-        for (HttpCookie cookie : cookies) {
-            var currentCookieName = cookie.getName();
-            if (currentCookieName.equals(cookieName)) {
-                ResponseCookie.ResponseCookieBuilder rcb = ResponseCookie.from(currentCookieName, "");
-                rcb.path("/");
-                rcb.maxAge(0);
-                response.addCookie(rcb.build());
+    fun deleteCookie(response: ServerHttpResponse, cookieMap: MultiValueMap<String?, HttpCookie>, cookieName: String) {
+        val cookies = cookieMap[cookieName]
+        for (cookie in cookies!!) {
+            val currentCookieName = cookie.name
+            if (currentCookieName == cookieName) {
+                val rcb = ResponseCookie.from(currentCookieName, "")
+                rcb.path("/")
+                rcb.maxAge(0)
+                response.addCookie(rcb.build())
             }
         }
     }
 
-    public void addCookie(ServerHttpResponse response, String cookieName, String cookieValue, String path, int age) {
-        ResponseCookie.ResponseCookieBuilder rcb = ResponseCookie.from(cookieName, cookieValue);
-        rcb.path("/");
-        rcb.httpOnly(true);
-        rcb.maxAge(120);
-        response.addCookie(rcb.build());
+    fun addCookie(response: ServerHttpResponse, cookieName: String?, cookieValue: String?, path: String?, age: Int) {
+        val rcb = ResponseCookie.from(cookieName, cookieValue)
+        rcb.path("/")
+        rcb.httpOnly(true)
+        rcb.maxAge(120)
+        response.addCookie(rcb.build())
     }
 }
